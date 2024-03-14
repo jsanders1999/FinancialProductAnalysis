b7b53c96c12df67dfd035f85417a567fa045f300
jsanders1999 <julian.arthur.sanders@gmail.com>
2024-03-14 09:11:16 +0100

    Run for many trackers

--- dataUtils.py
+++ dataUtils.py
@@ -1,5 +1,7 @@
+#Import standard libraries
 import numpy as np
 import scipy as sp
+from skewt_scipy.skewt import skewt
 import pandas as pd
 import matplotlib.pyplot as plt
 import yfinance as yf
@@ -7,6 +9,9 @@ import plotly.graph_objects as go
 import plotly.express as px
 from datetime import datetime, timedelta
 from plotly.subplots import make_subplots
+import seaborn as sns
+
+from plottingUtils import hist_with_fit
 
 def dr(df, products):
     for product in products:
@@ -34,7 +39,14 @@ def dr(df, products):
 
 def risk(dist, params, risk_free_rate=0.0):
     #TODO: integration by parts
-    return sp.integrate.quad(lambda x: x/(1+risk_free_rate)*dist(*params).pdf(x/(1+risk_free_rate)), -np.inf, 0)[0]
+    #res1 = (np.array(sp.integrate.quad(lambda x: (x/(1+risk_free_rate))*dist(*params).pdf(x/(1+risk_free_rate)), -1, 0))+
+    #        np.array(sp.integrate.quad(lambda x: (-1/(1+risk_free_rate))*dist(*params).pdf(x/(1+risk_free_rate)), -np.inf, -1)))
+            
+    res2 =  np.array(sp.integrate.quad(lambda x: -dist(*params).cdf(x/(1+risk_free_rate)), -1, 0, limit = 1000))
+    res2[0] = dist(*params).cdf(-1/(1+risk_free_rate)) + res2[0] + (-1/(1+risk_free_rate))*dist(*params).cdf(-1/(1+risk_free_rate))
+    #print("normal integral:", res1)
+    print("by parts:", res2)
+    return res2[0]#sp.integrate.quad(lambda x: x/(1+risk_free_rate)*dist(*params).pdf(x/(1+risk_free_rate)), -np.inf, 0)[0]
 
 def estimate_params(df, products, risk_free_rate = (1.033)**(1/365.25)-1):
     results = pd.DataFrame(columns=pd.Index(["mean", "std", "mean-std^2/2", "risk (R)", "mean-intrest (E)", "R/E"]))
@@ -43,6 +55,23 @@ def estimate_params(df, products, risk_free_rate = (1.033)**(1/365.25)-1):
         sigma = np.std(df["dr"][product])
         E = mu-sigma**2/2 - risk_free_rate
         R = risk(sp.stats.norm, (mu, sigma), risk_free_rate)
+        results.loc[product] = [mu, sigma, mu-sigma**2/2, R, E, R/E]
+    return results
+
+def fit_distribution(dist, data, bounds):
+    res = sp.stats.fit(dist, data, bounds = bounds)
+    return res, res.nllf()
+
+def estimate_params_dist(df, products, dist, bounds, risk_free_rate = (1.033)**(1/365.25)-1):
+    results = pd.DataFrame(columns=pd.Index(["mean", "std", "mean-std^2/2", "risk (R)", "mean-intrest (E)", "R/E"]))
+    for product in products:
+        #hist_with_fit(df["dr"][product].dropna(), [dist], [bounds], nbins = 150)
+        res, logL = fit_distribution(dist, df["dr"][product].dropna(), bounds)
+        print(res.params, logL)
+        mu = dist(*res.params).mean()
+        sigma = dist(*res.params).std()
+        E = mu-sigma**2/2 - risk_free_rate
+        R = risk(dist, res.params, risk_free_rate)
         results.loc[product] = [mu, sigma, mu-sigma**2/2, R, E, R/E]
     return results
 
--- plottingUtils.py
+++ plottingUtils.py
@@ -7,7 +7,6 @@ import plotly.graph_objects as go
 import plotly.express as px
 from datetime import datetime, timedelta
 from plotly.subplots import make_subplots
-
 import random
 
 def random_color():
@@ -49,4 +48,32 @@ def plot_log_candlesticks(df, products, normalize = True, remove_gaps = True, lo
         else:
             fig.update_yaxes(title_text="Stock price linear scale")
     fig.update_layout(height=800, width=1200)
+    fig.show()
+
+#temp
+def fit_distribution(dist, data, bounds):
+    res = sp.stats.fit(dist, data, bounds = bounds)
+    return res, res.nllf()
+
+def hist_with_fit(data, dists, bounds, nbins = 150):
+
+    fig = make_subplots(rows=2, cols=1, shared_yaxes='all', specs=[[{'type': 'box'}], [{'type': 'histogram'}]])
+    fig1 = px.histogram(data, nbins=nbins, marginal="box", histnorm="probability density")
+    fig.add_trace(go.Box(fig1.data[1]), row=1, col=1)
+    fig.add_trace(go.Histogram(fig1.data[0]), row=2, col=1)
+
+    for dist, bound in zip(dists, bounds):
+        res, logL = fit_distribution(dist, data, bounds = bound )
+        
+        fig2 = px.line(x=np.linspace(np.min(data)*1.1, np.max(data)*1.1, 500),
+                        y=dist.pdf(np.linspace(np.min(data)*1.1, np.max(data)*1.1, 500), *res.params))
+        fig2.update_traces(line_color=random_color())
+        #fig2.update_traces(name=f"{dist.name} fit, logL = {logL:.2f}")
+        fig.add_trace(go.Scatter(fig2.data[0]), secondary_y=False, row=2, col=1)
+
+    fig.update_layout(yaxis=dict(domain=[0.8516, 1.0]),
+                    yaxis2=dict(domain=[0.0, 0.8316],),
+                    xaxis=dict(showticklabels=False),                  
+                    )
+
     fig.show()
